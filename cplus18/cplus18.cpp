b713328834335e7f1f9a6595ef6ba493861d5c2e
Vetal82 <havevetal@gmail.com>
2024-04-25 20:43:35 +0200

    Create main

--- cplus18/cplus18.cpp
+++ cplus18/cplus18.cpp
@@ -1,11 +1,23 @@
 // cplus18.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include "Computer.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int N = 5;
+    Computer* arr = new Computer[N]
+    {
+        {"HP", 1.2, 6, false, 20'000},
+        {"Acer", 3.2, 8, true, 15'000},
+        {"Asus", 2.4, 16, false, 45'000},
+        {"Dell", 3.2, 8, true, 17'000},
+        {"Apple", 3.2, 32, false, 78'000},
+    };
+    for (int i = 0; i < N; i++)
+    {
+        arr[i].showInfo();
+    }
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
